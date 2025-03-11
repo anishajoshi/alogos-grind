a26595aa31e5814661187296845dfd75c97def88
Anisha Joshi <joshianisha15@gmail.com>
2025-03-11 03:17:26 -0400

    yuh

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -1,7 +1,34 @@
-# Valid Palindrome
+# Valid Palindrome - Easy . Two pointers
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+= 1
+            r -= 1
+        return True
+        
+
+   
+        '''
+        while L < R:
+         if s[L] == s[R]:
+           L += 1
+           R -= 1
+         else:
+          return False
+        '''
+        
+        
+        '''
         cleaned_str = ""
         for char in s:
             if char.isalnum():
@@ -11,6 +38,7 @@ class Solution:
         if cleaned_str == reversed_str:
             return True
         return False
+    '''
 
 def test():
     solution = Solution()
